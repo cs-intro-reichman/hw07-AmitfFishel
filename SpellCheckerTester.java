16eebba26e0196c10175d0120235c6c2f6421fae
AmitfFishel <154337319+AmitfFishel@users.noreply.github.com>
2024-01-31 18:14:37 +0200

    solved

--- SpellCheckerTester.java
+++ SpellCheckerTester.java
@@ -1,6 +1,22 @@
 public class SpellCheckerTester {
 
+    static String FILE_NAME = "dictionary.txt";
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java SpellCheckerTester <word> <threshold>");
+            return;
+        }
+
+        String word = args[0];
+        int threshold = Integer.parseInt(args[1]);
+
+        String[] dictionary = SpellChecker.readDictionary(FILE_NAME);
+        String closest = SpellChecker.spellChecker(word, threshold, dictionary);
+
+        System.out.println("Original Word: " + word);
+        System.out.println("Answer: " + closest);
+    }
+    /*public static void main(String[] args) {
         String word = args[0];
         int threshold = Integer.parseInt(args[1]);
         String[] dictionary = readDictionary("dictionary.txt");
@@ -56,7 +72,7 @@ public class SpellCheckerTester {
         }
 
         return  tempWord;
-    }
+    }*/
 
 }
 
